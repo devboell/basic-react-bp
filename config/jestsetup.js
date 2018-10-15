dbca97f2e58c8213104754228b600161b89c1111
devboell <devboell@gmail.com>
2018-10-15 16:01:08 +0200

    added enzyme adapter

--- config/jestsetup.js
+++ config/jestsetup.js
@@ -1,5 +1,10 @@
 import { shallow, render, mount } from 'enzyme'
 
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() })
+
 global.shallow = shallow
 global.render = render
 global.mount = mount
